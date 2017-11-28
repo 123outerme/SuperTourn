1f14ba285860f9da0eb0ba61481567877ba59c0a
Stephen P <123outerme@outlook.com>
2017-11-27 22:39:27 -0500

    Initial upload
    No functionality, gameplay, etc

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "outermeSDL.h"
+
+int main(int argc, char* argv[])
+{
+    initSDL("SuperTourn", ".", ".", 640, 480, 24);
+    printf("This is a test for SuperTourn, a new 2D fighting game and successor to calculator game. Thanks.");
+    return 0;
+}
