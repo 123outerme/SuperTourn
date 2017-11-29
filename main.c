b8208174d4e0e686883b73026d0163a9b2242c4e
Stephen Policelli <123outerme@outlook.com>
2017-11-28 22:20:42 -0500

    Fixed issues with loading IMGs

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include "outermeSDL.h"
 
+//Px437_ITT_BIOS_X.ttf
+
 int main(int argc, char* argv[])
 {
-    initSDL("SuperTourn", ".", ".", 640, 480, 24);
+    int ret;
+    printf("%d\n", ret = initSDL("SuperTourn", "x.xxx", "y.yyy", 640, 480, 24));
+    waitForKey();
     printf("This is a test for SuperTourn, a new 2D fighting game and successor to calculator game. Thanks.");
     return 0;
 }
