9a25f45749e3f384d1b3fc8804a7126cea3fa11f
Stephen Policelli <123outerme@outlook.com>
2017-11-29 19:29:57 -0500

    Added struct inits

--- superTourn.c
+++ superTourn.c
@@ -1,1 +1,35 @@
+#include "superTourn.h"
 
+void initHurtbox(hurtbox* boxptr, int relX, int relY, int w, int h, hurtboxType type)
+{
+    boxptr->relX = relX;
+    boxptr->relY = relY;
+    boxptr->w = w;
+    boxptr->h = h;
+    boxptr->hurtType = type;
+}
+
+void initHitbox(hitbox* boxptr, int x, int y, int w, int h, int frameOut, int activeLength, hitboxType type)
+{
+    boxptr->x = x;
+    boxptr->y = y;
+    boxptr->w = w;
+    boxptr->h = h;
+    boxptr->onFrame = 0;
+    boxptr->frameOut = frameOut;
+    boxptr->activeLength = activeLength;
+    boxptr->hitType = type;
+}
+
+void initModel(model* modelPtr, hurtbox* boxes)
+{
+    modelPtr->boxes = boxes;
+}
+
+void initAttack(attack* attackPtr, hitbox* boxes, int frameLength, int dmg, int kb)
+{
+    attackPtr->boxes = boxes;
+    attackPtr->frameLength = frameLength;
+    attackPtr->damage = dmg;
+    attackPtr->kb = kb;
+}
--- superTourn.h
+++ superTourn.h
@@ -3,17 +3,19 @@
 typedef enum {
     none,  //0
     grab,  //1
-    lowPriority,  //2
-    midPriority,  //3
-    highPriority,  //4
+    noPriority,  //2  -- only causes knockback on a sensitive hurtbox, has lowest priority
+    lowPriority,  //3  -- only causes knockback on sensitive/normal hurtboxes, only beats noPriority
+    midPriority,  //4  -- only causes knockback on sensitive/normal hurtboxes, only loses to highPriority
+    highPriority,  //5 -- causes knockback on all hurtboxes, doesn't lose
 } hitboxType;
 
 typedef enum {
-    sensititve,  //0
-    normal,  //1
-    superArmor,  //2
-    hyperArmor,  //3
-    invincible  //4
+    na,  //0
+    sensititve,  //1  -- increased state of damage/knockback gain and amount of hitboxes that can do knockback
+    normal,  //2  -- normal state of damage/knockback gain
+    superArmor,  //3  -- decreased state of knockback gain, same damage gain. Can't be knocked back by lowPriority attacks
+    hyperArmor,  //4  -- decreased state of both knockgack and damage gain. Can't be knocked back by midPriority attacks
+    invincible  //5  -- nothing can do damage or knockback except direct modifiactions to the values
 } hurtboxType;
 
 typedef struct {
@@ -21,5 +23,28 @@ typedef struct {
     int y;
     int w;
     int h;
-    hitboxType type;
+    int onFrame;
+    int frameOut;
+    int activeLength;
+    hitboxType hitType;
+
 } hitbox;
+
+typedef struct {
+    int relX;
+    int relY;
+    int w;
+    int h;
+    hurtboxType hurtType;
+} hurtbox;
+
+typedef struct {
+    hurtbox* boxes;  //hit/hurtboxes for the given model
+} model;
+
+typedef struct {
+    hitbox* boxes;
+    int frameLength;
+    int damage;
+    int knockback;
+} attack;
